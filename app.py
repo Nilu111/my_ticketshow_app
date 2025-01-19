0c5cfd75299b000dd58c74609387db30f0860e9b
Nilesh Sarkar <sarkarnilesh334@gmail.com>
2025-01-19 21:47:29 +0530

    19/01/25

--- app.py
+++ app.py
@@ -1,13 +1,15 @@
-from flask import Flask,render_template
+from flask import Flask
+from backend.models import db
 
 app=None
 
 def setup_app():
     app=Flask(__name__)
+    app.config["SQLALCHEMY_DATABASE_URI"]="sqlite:///ticket_shoq.sqlite3"   #having db file
+    db.init_app(app) #flask app connectednton db(sqlalchemy)
     app.app_context().push() #direct access to other models like database
-    # from models/sqlite connection
     app.debug=True
-    print("my_cine_world started...")\
+    print("my_cine_world started...")
     
 
 # call the app
@@ -18,5 +20,5 @@ setup_app()
 
 
 from backend.controller import *
-
-app.run()
+if __name__ =="__main__":
+    app.run()
--- backend/controller.py
+++ backend/controller.py
@@ -1,4 +1,5 @@
-from flask import Flask,render_template
+from flask import Flask,render_template,request
+from .models import *
 from flask import current_app as app
 
 
@@ -7,10 +8,34 @@ def home():
     return render_template('index.html')
 
 
-@app.route('/login')
+@app.route('/login' , methods=["GET","POST"])
 def login_page():
-    return render_template('login.html')
+    if request.method=="POST":
+        uname=request.form.get("username")
+        pwd=request.form.get("password")
+        usr=User_Info.query.filter_by(email=uname,password=pwd).first()
+        if usr and usr.role==0:
+            return render_template("admin_dashboard.html")
+        elif usr and usr.role==1:
+            return render_template("user_dashboard.html",user=usr.full_name)
+        else:
+            return render_template('login.html',msg="Invalid Credentials")
+    return render_template("login.html",msg="")
 
-@app.route('/signup')
+
+@app.route('/signup', methods=["GET","POST"])
 def sighup_page():
-    return render_template('signup.html')
+    if request.method=="POST":
+        uname=request.form.get("username")
+        pwd=request.form.get("password")
+        full_name=request.form.get("fullname")
+        address=request.form.get("location")
+        pin_code=request.form.get("pincode")
+        usr=User_Info.query.filter_by(email=uname).first()
+        if usr:
+            return render_template('signup.html',msg="user already existed")
+        new_usr=User_Info(email=uname,password=pwd,full_name=full_name,address=address,pin_code=pin_code)
+        db.session.add(new_usr)
+        db.session.commit()
+        return render_template('login.html',msg="Registration done successfully!")
+    return render_template('signup.html',msg="")
